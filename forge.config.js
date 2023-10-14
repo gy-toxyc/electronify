eecf793c850770375e2ac91cddddac8d0008900e
srtoxyc <ivi1000vm@gmail.com>
2023-10-15 01:25:18 +0200

    Ready to package

--- forge.config.js
+++ forge.config.js
@@ -4,19 +4,33 @@ module.exports = {
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        setupIcon: '/styles/img/electronify.ico'
+      },
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      platforms: ['darwin', 'linux', 'win32'],
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+        config: {
+          options: {
+          icon: '/styles/img/electronify.png'
+        }
+      },
     },
     {
-      name: '@electron-forge/maker-rpm',
-      config: {},
+      name: '@electron-forge/maker-dmg',
+      config: {
+        icon: '/styles/img/electronify.icns'
+      }
     },
+    {
+      name: '@electron-forge/maker-wix',
+      config: {
+        icon: '/styles/img/electronify.ico'
+      }
+    }
   ],
 };
