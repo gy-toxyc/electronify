40cfada4db9b93891fa3bffc75f5be64b9c98478
srtoxyc <ivi1000vm@gmail.com>
2023-10-15 01:54:10 +0200

    Maker configs stablished.

--- forge.config.js
+++ forge.config.js
@@ -1,36 +1,32 @@
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    icon: './style/img/electronify'
+  },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
       config: {
-        setupIcon: '/styles/img/electronify.ico'
+        setupIcon: './styles/img/electronify.ico'
       },
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin', 'linux', 'win32'],
     },
     {
       name: '@electron-forge/maker-deb',
         config: {
           options: {
-          icon: '/styles/img/electronify.png'
+          icon: './styles/img/electronify.png'
         }
       },
     },
     {
       name: '@electron-forge/maker-dmg',
       config: {
-        icon: '/styles/img/electronify.icns'
+        icon: './styles/img/electronify.icns'
       }
     },
     {
-      name: '@electron-forge/maker-wix',
-      config: {
-        icon: '/styles/img/electronify.ico'
-      }
-    }
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin', 'linux', 'win32'],
+    },
   ],
 };
